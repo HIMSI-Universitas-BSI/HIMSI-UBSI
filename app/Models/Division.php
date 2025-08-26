e28c64a2d0e6456fc23e0b7195a1ca7624a9cbf5
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-26 23:48:18 +0700

    buat table division

---
+++ app/Models/Division.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Division extends Model
+{
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'division';
+    protected $guarded = ['id'];
+    
+    protected $casts = [
+        'job_description' => 'array',
+    ];
+}
