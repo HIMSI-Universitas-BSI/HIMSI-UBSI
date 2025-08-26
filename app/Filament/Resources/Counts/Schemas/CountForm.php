5a666adb83877703238d2d59f0645cb3309105ea
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-27 00:27:05 +0700

    buat resource count

---
+++ app/Filament/Resources/Counts/Schemas/CountForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Counts\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class CountForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('digit')
+                    ->required(),
+                Toggle::make('active')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                TextInput::make('updated_by')
+                    ->numeric(),
+                TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+}
