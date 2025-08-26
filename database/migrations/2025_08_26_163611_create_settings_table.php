4248068946a248bad77adbe914a646f4a1c9d87f
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-26 23:39:26 +0700

    buat table setting

---
+++ database/migrations/2025_08_26_163611_create_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Traits\BaseModelSoftDeleteDefault;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDeleteDefault;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('setting', function (Blueprint $table) {
+            $table->id();
+            $table->string('key', 128);
+            $table->text('value');
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('setting');
+    }
+};
