c8ed84c5c7ce08f213902a0c4b7b6a34747b434e
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-27 11:19:02 +0700

    ubah nav label about

--- app/Filament/Resources/Abouts/AboutResource.php
+++ app/Filament/Resources/Abouts/AboutResource.php
@@ -20,7 +20,11 @@ class AboutResource extends Resource
 {
     protected static ?string $model = About::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Sparkles;
+
+    protected static ?string $navigationLabel = 'About';
+
+    protected static ?string $pluralModelLabel = 'About';
 
     public static function form(Schema $schema): Schema
     {
