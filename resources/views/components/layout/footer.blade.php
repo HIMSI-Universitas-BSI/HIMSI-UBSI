de2ccf8ca65e55b27167989e9ef76334bef2938f
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-27 12:15:03 +0700

    tambah tamplate footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,75 +1,70 @@
-<!-- footer -->
-<footer class="bg-gray-200 opacity-70">
-    <div class="container pt-8 xl:pt-10 lg:pt-10 md:pt-10 pb-7">
-        <div class="flex flex-wrap mx-[-15px] xl:mx-0 lg:mx-0 !mt-[-30px]">
-            <div class="xl:w-4/12 lg:w-4/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:px-0 lg:px-0 !mt-[30px]">
+<footer class=" bg-[#21262c] opacity-100  !text-[#cacaca]">
+        <div class="container-fluid pt-20 xl:pt-28 lg:pt-28 md:pt-28 pb-16 xl:pb-20 lg:pb-20 md:pb-20 xxl:!px-10">
+        <footer class=" bg-[#21262c] opacity-100  !text-[#cacaca]">
+            <div class="container pt-[4.5rem] xl:pt-[7rem] lg:pt-[7rem] md:pt-[7rem] pb-[1.75rem]">
+            <div class="flex flex-wrap mx-[-15px] !mt-[-30px] xl:!mt-0 lg:!mt-0">
+                <div class="xl:w-4/12 lg:w-4/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
                 <div class="widget">
-                    <img src="{{ asset('assets/img/Logo_Jasanya.png') }}"
-                        srcset="{{ asset('assets/img/Logo_Jasanya.png') }} 2x" alt="image">
-                    <p class="lead text-[0.9rem] pt-4 font-medium !leading-[1.65] !mb-0 text-gray-700">We are trusted by over 5000+
-                        clients. Join them by using our services and grow your business.</p>
+                    <h3 class="h2 !mb-3 !text-white !leading-[1.35]">Join the Community</h3>
+                    <p class="lead !leading-[1.65] text-[0.9rem] font-medium !mb-5">Let's make something great together. We are trusted by over 5000+ clients. Join them by using our services and grow your business.</p>
+                    <a href="#" class="btn btn-white  !rounded-[50rem]">Join Us</a>
                 </div>
                 <!-- /.widget -->
-            </div>
-            <!-- /column -->
-            <div
-                class="xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!ml-[16.66666667%] lg:!ml-[16.66666667%] xl:px-0 lg:px-0 !mt-[30px]">
+                </div>
+                <!-- /column -->
+                <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full lg:!ml-[16.66666667%] xl:!ml-[16.66666667%] max-lg:!mt-[30px]">
                 <div class="widget">
-                    <div class="flex flex-row">
-                        <div>
-                            <div
-                                class="icon !text-[#605dba] xl:!text-[1.4rem] !text-[calc(1.265rem_+_0.18vw)] !mr-4 !mt-[-0.25rem]">
-                                <i class="uil uil-phone-volume before:content-['\ec50']"></i>
-                            </div>
-                        </div>
-                        <div>
-                            <h5 class="!mb-1">Phone / Wa</h5>
-                            <p class="!mb-0 text-gray-700">//</p>
-                        </div>
-                    </div>
-                    <!--/div -->
+                    <h4 class="widget-title !text-white !mb-3">Need Help?</h4>
+                    <ul class="pl-0 list-none text-inherit  !mb-0">
+        <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Support</a></li>
+        <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Get Started</a></li>
+        <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Terms of Use</a></li>
+        <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Privacy Policy</a></li>
+                    </ul>
                 </div>
                 <!-- /.widget -->
-            </div>
-            <!-- /column -->
-            <div class="xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:px-0 lg:px-0 !mt-[30px]">
+                </div>
+                <!-- /column -->
+                <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
                 <div class="widget">
-                    <div class="flex flex-row">
-                        <div>
-                            <div
-                                class="icon !text-[#605dba] xl:!text-[1.4rem] !text-[calc(1.265rem_+_0.18vw)] !mr-4 !mt-[-0.25rem]">
-                                <i class="uil uil-location-pin-alt before:content-['\ebd8']"></i>
-                            </div>
-                        </div>
-                        <div class="!self-start !justify-start">
-                            <h5 class="!mb-1">Address</h5>
-                            <address class=" not-italic !leading-[inherit] !mb-4 text-gray-700">//</address>
-                        </div>
-                    </div>
-                    <!--/div -->
+                    <h4 class="widget-title !text-white !mb-3">Learn More</h4>
+                    <ul class="pl-0 list-none   !mb-0">
+                <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">About Us</a></li>
+                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Our Story</a></li>
+                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Projects</a></li>
+                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Pricing</a></li>
+                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Features</a></li>
+                    </ul>
                 </div>
                 <!-- /.widget -->
+                </div>
+                <!-- /column -->
+                <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
+                <div class="widget">
+                    <h4 class="widget-title !text-white !mb-3">Get in Touch</h4>
+                    <address class=" not-italic !leading-[inherit] !mb-4">Moonshine St. 14/05 Light City, London, United Kingdom</address>
+                    <a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="mailto:first.last@email.com">info@email.com</a><br> 00 (123) 456 78 90
+                </div>
+                <!-- /.widget -->
+                </div>
+                <!-- /column -->
             </div>
-            <!-- /column -->
+            <!--/.row -->
+            <hr class="!mt-13 xl:!mt-20 lg:!mt-20 md:!mt-20 !mb-7">
+            <div class="xl:!flex lg:!flex md:!flex items-center justify-between">
+                <p class="!mb-2 xl:!mb-0 lg:!mb-0">© 2024 Sandbox. All rights reserved.</p>
+                <nav class="nav social social-white  xl:!text-right lg:!text-right md:!text-right">
+                <a class="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]" href="#"><i class="uil uil-twitter before:content-['\ed59'] text-[1rem] !text-[#5daed5]"></i></a>
+                <a class="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]" href="#"><i class="uil uil-facebook-f before:content-['\eae2'] text-[1rem] !text-[#4470cf]"></i></a>
+                <a class="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]" href="#"><i class="uil uil-dribbble before:content-['\eaa2'] text-[1rem] !text-[#e94d88]"></i></a>
+                <a class="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]" href="#"><i class="uil uil-instagram before:content-['\eb9c'] text-[1rem] !text-[#d53581]"></i></a>
+                <a class="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]" href="#"><i class="uil uil-youtube before:content-['\edb5'] text-[1rem] !text-[#c8312b]"></i></a>
+                </nav>
+                <!-- /.social -->
+            </div>
+            <!-- /div -->
+            </div>
+            <!-- /.container -->
+        </footer>
         </div>
-        <!--/.row -->
-        <hr class="!mt-13 xl:!mt-[4.5rem] lg:!mt-[4.5rem] md:!mt-[4.5rem] !mb-7">
-        <div class="xl:!flex lg:!flex md:!flex items-center justify-between">
-            <p class="!mb-2 xl:!mb-0 lg:!mb-0">© 2025 Jasanya Tech. All rights reserved.</p>
-            <nav class="nav social social-muted  !mb-0  xl:!text-right lg:!text-right md:!text-right">
-                <a class="m-[0_0_0_.7rem] max-md:m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]"
-                    href="#"><i
-                        class="uil uil-facebook-f before:content-['\eae2'] text-[1rem] !text-[#4470cf]"></i></a>
-                <a class="m-[0_0_0_.7rem] max-md:m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]"
-                    href="#"><i
-                        class="uil uil-instagram before:content-['\eb9c'] text-[1rem] !text-[#d53581]"></i></a>
-                <a class="m-[0_0_0_.7rem] max-md:m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]"
-                    href="#"><i
-                        class="uil uil-youtube before:content-['\edb5'] text-[1rem] !text-[#c8312b]"></i></a>
-            </nav>
-            <!-- /.social -->
-        </div>
-    </div>
-    <!-- /.container -->
 </footer>
-<!-- progress wrapper -->
