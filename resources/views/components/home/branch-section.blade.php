6bd24d6faca6047499f0d65b2d04e5f1b7b1c9bc
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-28 23:12:38 +0700

    get data di section cabang

--- resources/views/components/home/branch-section.blade.php
+++ resources/views/components/home/branch-section.blade.php
@@ -11,77 +11,37 @@
     <div class="swiper-container grid-view !mb-6" data-margin="30" data-dots="true" data-items-xl="3" data-items-md="2" data-items-xs="1">
         <div class="swiper">
         <div class="swiper-wrapper">
-            <div class="swiper-slide group">
-            <figure class="!rounded-[.4rem] !mb-6"><img class="!rounded-[.4rem]" src="../../assets/img/photos/pd7.jpg" srcset="../../assets/img/photos/pd7@2x.jpg 2x" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="../../assets/img/photos/pd7-full.jpg" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
-            <div class="project-details flex justify-center flex-col">
-                <div class="post-header">
-                <h2 class="post-title h3"><a href="../../single-project.html" class="!text-[#343f52] hover:!text-[#3f78e0]">Cras Fermentum Sem</a></h2>
-                <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">Stationary</div>
+            
+            @foreach ( $branches as $b )
+                <div class="swiper-slide group">
+                    <figure class="!rounded-[.4rem] !mb-6">
+                        <img 
+                            class="!rounded-[.4rem] h-[400px] w-[200px] object-cover" 
+                            src="{{ asset('storage/' . $b->poster) }}" 
+                            alt="image">
+                            @foreach ($b->image as $img)
+                                @if (!empty($img))
+                                    <a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" 
+                                    href="{{ asset('storage/' . $img) }}" 
+                                    data-glightbox 
+                                    data-gallery="projects-group">
+                                        <i class="uil uil-focus-add before:content-['\eb22']"></i>
+                                    </a>
+                                @endif
+                            @endforeach
+                    </figure>
+                    <div class="project-details flex justify-center flex-col">
+                        <div class="post-header">
+                        <h2 class="post-title h3"><a href="../../single-project.html" class="!text-[#343f52] hover:!text-[#3f78e0]">{{ $b->name }}</a></h2>
+                        <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">{{ $b->location }}</div>
+                        </div>
+                        <!-- /.post-header -->
+                    </div>
+                <!-- /.project-details -->
                 </div>
-                <!-- /.post-header -->
-            </div>
-            <!-- /.project-details -->
-            </div>
-            <!--/.swiper-slide -->
-            <div class="swiper-slide group">
-            <figure class="!rounded-[.4rem] !mb-6"><img class="!rounded-[.4rem]" src="../../assets/img/photos/pd8.jpg" srcset="../../assets/img/photos/pd8@2x.jpg 2x" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="../../assets/img/photos/pd8-full.jpg" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
-            <div class="project-details flex justify-center flex-col">
-                <div class="post-header">
-                <h2 class="post-title h3"><a href="../../single-project2.html" class="!text-[#343f52] hover:!text-[#3f78e0]">Mollis Ipsum Mattis</a></h2>
-                <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">Magazine, Book</div>
-                </div>
-                <!-- /.post-header -->
-            </div>
-            <!-- /.project-details -->
-            </div>
-            <!--/.swiper-slide -->
-            <div class="swiper-slide group">
-            <figure class="!rounded-[.4rem] !mb-6"><img class="!rounded-[.4rem]" src="../../assets/img/photos/pd9.jpg" srcset="../../assets/img/photos/pd9@2x.jpg 2x" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="../../assets/img/photos/pd9-full.jpg" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
-            <div class="project-details flex justify-center flex-col">
-                <div class="post-header">
-                <h2 class="post-title h3"><a href="../../single-project3.html" class="!text-[#343f52] hover:!text-[#3f78e0]">Ipsum Ultricies Cursus</a></h2>
-                <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">Packaging</div>
-                </div>
-                <!-- /.post-header -->
-            </div>
-            <!-- /.project-details -->
-            </div>
-            <!--/.swiper-slide -->
-            <div class="swiper-slide group">
-            <figure class="!rounded-[.4rem] !mb-6"><img class="!rounded-[.4rem]" src="../../assets/img/photos/pd10.jpg" srcset="../../assets/img/photos/pd10@2x.jpg 2x" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="../../assets/img/photos/pd10-full.jpg" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
-            <div class="project-details flex justify-center flex-col">
-                <div class="post-header">
-                <h2 class="post-title h3"><a href="../../single-project.html" class="!text-[#343f52] hover:!text-[#3f78e0]">Inceptos Euismod Egestas</a></h2>
-                <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">Stationary, Branding</div>
-                </div>
-                <!-- /.post-header -->
-            </div>
-            <!-- /.project-details -->
-            </div>
-            <!--/.swiper-slide -->
-            <div class="swiper-slide group">
-            <figure class="!rounded-[.4rem] !mb-6"><img class="!rounded-[.4rem]" src="../../assets/img/photos/pd11.jpg" srcset="../../assets/img/photos/pd11@2x.jpg 2x" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="../../assets/img/photos/pd11-full.jpg" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
-            <div class="project-details flex justify-center flex-col">
-                <div class="post-header">
-                <h2 class="post-title h3"><a href="../../single-project2.html" class="!text-[#343f52] hover:!text-[#3f78e0]">Ipsum Mollis Vulputate</a></h2>
-                <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">Packaging</div>
-                </div>
-                <!-- /.post-header -->
-            </div>
-            <!-- /.project-details -->
-            </div>
-            <!--/.swiper-slide -->
-            <div class="swiper-slide group">
-            <figure class="!rounded-[.4rem] !mb-6"><img class="!rounded-[.4rem]" src="../../assets/img/photos/pd12.jpg" srcset="../../assets/img/photos/pd12@2x.jpg 2x" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="../../assets/img/photos/pd12-full.jpg" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
-            <div class="project-details flex justify-center flex-col">
-                <div class="post-header">
-                <h2 class="post-title h3"><a href="../../single-project3.html" class="!text-[#343f52] hover:!text-[#3f78e0]">Porta Ornare Cras</a></h2>
-                <div class="uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !mb-[0.4rem] !text-[#9499a3]">Branding</div>
-                </div>
-                <!-- /.post-header -->
-            </div>
-            <!-- /.project-details -->
-            </div>
+                <!--/.swiper-slide -->
+            @endforeach
+
             <!--/.swiper-slide -->
         </div>
         <!--/.swiper-wrapper -->
