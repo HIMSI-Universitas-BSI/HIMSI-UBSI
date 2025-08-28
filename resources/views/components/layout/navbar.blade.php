0adce22ceee66925ae090afdd91243c877734de2
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-28 13:41:37 +0700

    fix navbar

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -8,121 +8,39 @@
             </div>
             <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
                 <div class="offcanvas-header xl:!hidden lg:!hidden">
-                    <h3 class="text-white xl:!text-[1.5rem] !text-[calc(1.275rem_+_0.3vw)]  !mb-0">Sandbox</h3>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body xl:!ml-auto lg:!ml-auto flex  flex-col !h-full">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-                    <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Dropdown</a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-item"><a class="dropdown-item" href="#">Action</a></li>
-                        <li class="dropdown dropdown-submenu dropend"><a class="dropdown-item dropdown-toggle" href="#" data-bs-toggle="dropdown">Dropdown</a>
-                        <ul class="dropdown-menu">
-                            <li class="dropdown dropdown-submenu dropend"><a class="dropdown-item dropdown-toggle" href="#" data-bs-toggle="dropdown">Dropdown</a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                        
+                        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Divisi</a>
                             <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="dropdown-item" href="#">Action</a></li>
-                                <li class="nav-item"><a class="dropdown-item" href="#">Another Action</a></li>
+                                
+                                <li class="nav-item"><a class="dropdown-item" href="#">Pendidikan</a></li>
+                                <li class="nav-item"><a class="dropdown-item" href="#">Kominfo</a></li>
+                                <li class="nav-item"><a class="dropdown-item" href="#">RSDM</a></li>
+                                <li class="nav-item"><a class="dropdown-item" href="#">Litbang</a></li>
+                                
                             </ul>
-                            </li>
-                            <li class="nav-item"><a class="dropdown-item" href="#">Action</a></li>
-                            <li class="nav-item"><a class="dropdown-item" href="#">Another Action</a></li>
-                        </ul>
                         </li>
-                        <li class="nav-item"><a class="dropdown-item" href="#">Another Action</a></li>
+
+                        <li class="nav-item"><a class="nav-link" href="#">Galery</a></li>
+                        
+                        <div class="offcanvas-footer xl:!hidden lg:!hidden">
+                        </div>
+                        <!-- /.offcanvas-footer -->
                     </ul>
-                    </li>
-                    <li class="nav-item dropdown dropdown-mega"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Mega Menu</a>
-                    <ul class="dropdown-menu mega-menu">
-                        <li class="mega-menu-content">
-                        <div class="flex flex-wrap mx-[-15px] mx-0 xl:mx-[-7.5px] lg:mx-[-7.5px]">
-                            <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!px-[7.5px] lg:!px-[7.5px]">
-                            <h6 class="dropdown-header">One</h6>
-                            <div class="flex flex-wrap mx-0">
-                                <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] max-w-full">
-                                <ul class="!pl-0 list-none">
-                                    <li><a class="dropdown-item" href="#">Link</a></li>
-                                    <li><a class="dropdown-item" href="#">Link</a></li>
-                                    <li><a class="dropdown-item" href="#">Link</a></li>
-                                </ul>
-                                </div>
-                                <!--/column -->
-                                <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] max-w-full">
-                                <ul class="!pl-0 list-none">
-                                    <li><a class="dropdown-item" href="#">Link</a></li>
-                                    <li><a class="dropdown-item" href="#">Link</a></li>
-                                    <li><a class="dropdown-item" href="#">Link</a></li>
-                                </ul>
-                                </div>
-                                <!--/column -->
-                            </div>
-                            <!--/.row -->
-                            </div>
-                            <!--/column -->
-                            <div class="xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!px-[7.5px] lg:!px-[7.5px]">
-                            <h6 class="dropdown-header">Two</h6>
-                            <ul class="!pl-0 list-none">
-                                <li><a class="dropdown-item" href="#">Link</a></li>
-                                <li><a class="dropdown-item" href="#">Link</a></li>
-                                <li><a class="dropdown-item" href="#">Link</a></li>
-                            </ul>
-                            </div>
-                            <!--/column -->
-                            <div class="xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!px-[7.5px] lg:!px-[7.5px]">
-                            <h6 class="dropdown-header">Three</h6>
-                            <ul class="!pl-0 list-none">
-                                <li><a class="dropdown-item" href="#">Link</a></li>
-                                <li><a class="dropdown-item" href="#">Link</a></li>
-                                <li><a class="dropdown-item" href="#">Link</a></li>
-                            </ul>
-                            </div>
-                            <!--/column -->
-                        </div>
-                        <!--/.row -->
-                        </li>
-                        <!--/.mega-menu-content-->
-                    </ul>
-                    <!--/.dropdown-menu -->
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Dropdown Large</a>
-                    <div class="dropdown-menu dropdown-lg">
-                        <div class="dropdown-lg-content">
-                        <div>
-                            <h6 class="dropdown-header">One</h6>
-                            <ul class="!pl-0 list-none">
-                            <li><a class="dropdown-item" href="#">Link</a></li>
-                            <li><a class="dropdown-item" href="#">Link</a></li>
-                            <li><a class="dropdown-item" href="#">Another Link</a></li>
-                            </ul>
-                        </div>
-                        <!-- /.column -->
-                        <div>
-                            <h6 class="dropdown-header">Two</h6>
-                            <ul class="!pl-0 list-none">
-                            <li><a class="dropdown-item" href="#">Link</a></li>
-                            <li><a class="dropdown-item" href="#">Link</a></li>
-                            <li><a class="dropdown-item" href="#">Another Link</a></li>
-                            </ul>
-                        </div>
-                        <!-- /.column -->
-                        </div>
-                        <!-- /auto-column -->
-                    </div>
-                    </li>
-                </ul>
-                <!-- /.navbar-nav -->
-                <div class="offcanvas-footer xl:!hidden lg:!hidden">
-                </div>
-                <!-- /.offcanvas-footer -->
                 </div>
                 <!-- /.offcanvas-body -->
             </div>
+
             <!-- /.navbar-collapse -->
             <div class="navbar-other w-full !flex !ml-auto">
                 <ul class="navbar-nav !flex-row !items-center !ml-auto">
                     <li class="nav-item hidden xl:block lg:block md:block">
-                        <a href="../../contact.html" class="btn btn-sm btn-primary !text-white !bg-[#3f78e0] border-[#3f78e0] hover:text-white hover:bg-[#3f78e0] hover:!border-[#3f78e0]   active:text-white active:bg-[#3f78e0] active:border-[#3f78e0] disabled:text-white disabled:bg-[#3f78e0] disabled:border-[#3f78e0] !rounded-[50rem]">Contact</a>
+                        <a href="../../contact.html" class="btn btn-sm btn-primary !text-white !bg-[#3f78e0] border-[#3f78e0] hover:text-white hover:bg-[#3f78e0] hover:!border-[#3f78e0]   active:text-white active:bg-[#3f78e0] active:border-[#3f78e0] disabled:text-white disabled:bg-[#3f78e0] disabled:border-[#3f78e0] !rounded-[50rem]">Daftar Himsi</a>
                     </li>
                     <li class="nav-item xl:!hidden lg:!hidden">
                         <button class="hamburger offcanvas-nav-btn"><span></span></button>
@@ -131,6 +49,7 @@
                 <!-- /.navbar-nav -->
             </div>
             <!-- /.navbar-other -->
+
         </div>
         <!-- /.navbar-collapse-wrapper -->
     </div>
