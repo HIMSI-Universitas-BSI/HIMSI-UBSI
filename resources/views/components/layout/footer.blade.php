0e5b49688b43d962e7e0015acbef9d93518fa31f
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-28 14:05:56 +0700

    ubah nav di footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -3,53 +3,59 @@
         <footer class=" bg-[#1B3C53] opacity-100  !text-[#cacaca]">
             <div class="container pt-[4.5rem] xl:pt-[7rem] lg:pt-[7rem] md:pt-[7rem] pb-[1.75rem]">
             <div class="flex flex-wrap mx-[-15px] !mt-[-30px] xl:!mt-0 lg:!mt-0">
+                
                 <div class="xl:w-4/12 lg:w-4/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
-                <div class="widget">
-                    <h3 class="h2 !mb-3 !text-white !leading-[1.35]">Join the Community</h3>
-                    <p class="lead !leading-[1.65] text-[0.9rem] font-medium !mb-5">Let's make something great together. We are trusted by over 5000+ clients. Join them by using our services and grow your business.</p>
-                    <a href="#" class="btn btn-white  !rounded-[50rem]">Join Us</a>
-                </div>
+                    <div class="widget">
+                        <h3 class="h2 !mb-3 !text-white !leading-[1.35]">Join the Community</h3>
+                        <p class="lead !leading-[1.65] text-[0.9rem] font-medium !mb-5">Let's make something great together. We are trusted by over 5000+ clients. Join them by using our services and grow your business.</p>
+                        <a href="#" class="btn btn-white  !rounded-[50rem]">Join Us</a>
+                    </div>
                 <!-- /.widget -->
                 </div>
+
                 <!-- /column -->
                 <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full lg:!ml-[16.66666667%] xl:!ml-[16.66666667%] max-lg:!mt-[30px]">
-                <div class="widget">
-                    <h4 class="widget-title !text-white !mb-3">Need Help?</h4>
-                    <ul class="pl-0 list-none text-inherit  !mb-0">
-        <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Support</a></li>
-        <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Get Started</a></li>
-        <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Terms of Use</a></li>
-        <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
+                    <div class="widget">
+                        <h4 class="widget-title !text-white !mb-3">Sosial Media</h4>
+                        <ul class="pl-0 list-none text-inherit  !mb-0">
+                            <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Instagram</a></li>
+                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Tiktok</a></li>
+                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Linkedin</a></li>
+                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Youtube</a></li>
+                        </ul>
+                    </div>
                 <!-- /.widget -->
                 </div>
+
                 <!-- /column -->
                 <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
-                <div class="widget">
-                    <h4 class="widget-title !text-white !mb-3">Learn More</h4>
-                    <ul class="pl-0 list-none   !mb-0">
-                <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">About Us</a></li>
-                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Our Story</a></li>
-                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Projects</a></li>
-                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Pricing</a></li>
-                <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Features</a></li>
-                    </ul>
-                </div>
+                    <div class="widget">
+                        <h4 class="widget-title !text-white !mb-3">Halaman</h4>
+                        <ul class="pl-0 list-none   !mb-0">
+                            <li><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Tentang Kami</a></li>
+                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Divisi</a></li>
+                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Galeri</a></li>
+                            {{-- <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Pricing</a></li>
+                            <li class="!mt-[0.35rem]"><a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="#">Features</a></li> --}}
+                        </ul>
+                    </div>
                 <!-- /.widget -->
                 </div>
+
                 <!-- /column -->
                 <div class="md:w-4/12 lg:w-2/12 xl:w-2/12 w-full flex-[0_0_auto] !px-[15px] max-w-full max-lg:!mt-[30px]">
-                <div class="widget">
-                    <h4 class="widget-title !text-white !mb-3">Get in Touch</h4>
-                    <address class=" not-italic !leading-[inherit] !mb-4">Moonshine St. 14/05 Light City, London, United Kingdom</address>
-                    <a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="mailto:first.last@email.com">info@email.com</a><br> 00 (123) 456 78 90
-                </div>
+                    <div class="widget">
+                        <h4 class="widget-title !text-white !mb-3">Kontak</h4>
+                        <address class=" not-italic !leading-[inherit] !mb-4">Jl. Kramat Raya No.98, RT.2/RW.9, Kwitang, Kec. Senen, Kota Jakarta Pusat</address>
+                        <a class="!text-[#cacaca] hover:!text-[#3f78e0]" href="mailto:first.last@email.com">himsi@bsi.ac.id</a><br> (021) 23231170
+                    </div>
                 <!-- /.widget -->
                 </div>
                 <!-- /column -->
+
             </div>
             <!--/.row -->
+
             <hr class="!mt-13 xl:!mt-20 lg:!mt-20 md:!mt-20 !mb-7">
             <div class="xl:!flex lg:!flex md:!flex items-center justify-between">
                 <p class="!mb-2 xl:!mb-0 lg:!mb-0">© 2025 Himsi UBSI. All rights reserved.</p>
