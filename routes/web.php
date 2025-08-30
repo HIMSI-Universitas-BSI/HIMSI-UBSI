0bf6b5d547f0040f3b7ad057e77aa2346b15e5a2
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-30 15:26:59 +0700

    buat method dan controller blog

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
     
 Route::get('/', function () {
@@ -8,3 +9,5 @@ Route::get('/', function () {
 });
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/', [BlogController::class, 'index']);
