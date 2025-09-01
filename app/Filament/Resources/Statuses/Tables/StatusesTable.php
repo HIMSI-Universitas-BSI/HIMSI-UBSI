b56f8aaca357c89e63d97d35ca3b96abaca2a3e0
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-01 23:52:24 +0700

    edit table status

--- app/Filament/Resources/Statuses/Tables/StatusesTable.php
+++ app/Filament/Resources/Statuses/Tables/StatusesTable.php
@@ -20,17 +20,16 @@ class StatusesTable
             ->columns([
                 TextColumn::make('name')
                     ->searchable(),
+                TextColumn::make('description')
+                    ->searchable(),
                 IconColumn::make('active')
                     ->boolean(),
-                TextColumn::make('created_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('updated_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('deleted_by')
-                    ->numeric()
-                    ->sortable(),
+                TextColumn::make('createdBy.name')
+                    ->label('Created By'),
+                TextColumn::make('updatedBy.name')
+                    ->label("Updated by"),
+                TextColumn::make('deletedBy.name')
+                    ->label("Deleted by"),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
