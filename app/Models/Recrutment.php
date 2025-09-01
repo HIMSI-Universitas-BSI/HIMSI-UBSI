4fbe0ccd8795b65b85e3eeda9896459c7b85908c
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-01 19:20:17 +0700

    buat table recrutment

---
+++ app/Models/Recrutment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Recrutment extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+    protected $table = 'recrutment';
+    protected $guarded = ['id'];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class, 'branch_id');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+}
