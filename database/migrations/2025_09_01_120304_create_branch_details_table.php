f7e7a0ef2c9ff073d9fde2840506c7caaa650f7d
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-01 19:10:15 +0700

    buat table branch detail

---
+++ database/migrations/2025_09_01_120304_create_branch_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('branch_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('branch_id')->constrained('branch');
+            $table->json('ketua');
+            $table->json('wakil_ketua');
+            $table->json('sekertaris_1');
+            $table->json('sekertaris_2');
+            $table->json('bendahara_1');
+            $table->json('bendahara_2');
+            $table->json('koor_pendidikan');
+            $table->json('koor_kominfo');
+            $table->json('koor_rsdm');
+            $table->json('koor_litbang');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('branch_detail');
+    }
+};
