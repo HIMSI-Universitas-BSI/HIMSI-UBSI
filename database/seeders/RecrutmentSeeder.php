98837243cd57cb0b141d7334d518b2ba103644bb
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-02 17:16:14 +0700

    buat seeder recrutment

---
+++ database/seeders/RecrutmentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Recrutment;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RecrutmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Recrutment::factory()->count(100)->create();
+    }
+}
