bb699b787bf717346f4f2f944e99b566bec7e7c4
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-02 13:58:36 +0700

    edit form recrutmant

--- app/Filament/Resources/Recrutments/Schemas/RecrutmentForm.php
+++ app/Filament/Resources/Recrutments/Schemas/RecrutmentForm.php
@@ -2,9 +2,15 @@
 
 namespace App\Filament\Resources\Recrutments\Schemas;
 
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
+use App\Models\Branch;
+use App\Models\Status;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Forms\Components\FileUpload;
 
 class RecrutmentForm
 {
@@ -13,33 +19,80 @@ class RecrutmentForm
         return $schema
             ->components([
                 TextInput::make('nim')
+                    ->label('NIM')
+                    ->numeric()
+                    ->maxLength(8)
                     ->required(),
                 TextInput::make('name')
+                    ->label('Nama Lengkap')
                     ->required(),
-                TextInput::make('semester')
-                    ->required(),
-                TextInput::make('ektm')
+                Select::make('semester')
+                    ->label('Semester')
+                    ->options([
+                        'Semester 1' => 'Semester 1',
+                        'Semester 2' => 'Semester 2',
+                        'Semester 3' => 'Semester 3',
+                        'Semester 4' => 'Semester 4',
+                    ])
                     ->required(),
                 TextInput::make('email')
-                    ->label('Email address')
+                    ->label('Email')
                     ->email()
                     ->required(),
                 TextInput::make('instagram')
                     ->required(),
                 TextInput::make('no_wa')
+                    ->label('No WhatsApp')
+                    ->helperText('Masukkan No WhatsApp aktif, di awali dengan 62')
                     ->required(),
+                FileUpload::make('ektm')
+                    ->required()
+                    ->label('EKTM')
+                    ->image()
+                    ->disk('public')
+                    ->directory('ektm')
+                    ->columnSpanFull()
+                    ->helperText('Screenshot halaman beranda pada aplikasi BSI ID'),
                 Textarea::make('description')
+                    ->label('Alasan ingin bergabung dengan HIMSI')
                     ->required()
                     ->columnSpanFull(),
-                TextInput::make('branch_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('follow_dpc')
-                    ->required(),
-                TextInput::make('cv'),
-                TextInput::make('status_id')
-                    ->required()
-                    ->numeric(),
+
+                Section::make('Cabang Himsi')
+                    ->description('Data Dewan Pimpinan Cabang (DPC) HIMSI UBSI')
+                    ->icon(Heroicon::BuildingOffice2)
+                    ->schema([
+                        Select::make('branch_id')
+                            ->required()
+                            ->label('Cabang / DPC')
+                            ->options(Branch::all()->pluck('name', 'id'))
+                            ->reactive() // wajib biar bisa trigger perubahan
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                if ($state) {
+                                    $branch = \App\Models\Branch::find($state);
+                                    $set('instagram', $branch?->instagram); 
+                                } else {
+                                    $set('instagram', null);
+                                }
+                            }),
+                        TextInput::make('instagram'),
+                        FileUpload::make('follow_dpc')
+                            ->label('Bukti Follow Instagram DPC HIMSI')
+                            ->required()
+                            ->disk('public')
+                            ->directory('follow_dpc'),
+                    ])->columnSpanFull(),
+
+                TextInput::make('cv')
+                    ->label('Curriculum Vitae / CV')
+                    ->columnSpanFull()
+                    ->helperText('Mohon Masukan Link Drive Yang Berisi CV Anda'),
+                
+                Select::make('status_id')
+                    ->default(Status::find(1)->id)
+                    ->label('Status')
+                    ->options(Status::all()->pluck('name', 'id'))
+                    ->visible(auth()->id() === 1),
             ]);
     }
 }
