cb53edf7027839e03ecccb5398909da7a2a521d5
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-02 14:09:11 +0700

    edit infolist recrutment

--- app/Filament/Resources/Recrutments/Schemas/RecrutmentInfolist.php
+++ app/Filament/Resources/Recrutments/Schemas/RecrutmentInfolist.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Resources\Recrutments\Schemas;
 
-use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
+use Filament\Infolists\Components\TextEntry;
 
 class RecrutmentInfolist
 {
@@ -11,26 +15,58 @@ class RecrutmentInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('nim'),
-                TextEntry::make('name'),
-                TextEntry::make('semester'),
-                TextEntry::make('ektm'),
-                TextEntry::make('email')
-                    ->label('Email address'),
-                TextEntry::make('instagram'),
-                TextEntry::make('no_wa'),
-                TextEntry::make('branch_id')
-                    ->numeric(),
-                TextEntry::make('follow_dpc'),
-                TextEntry::make('cv'),
-                TextEntry::make('status_id')
-                    ->numeric(),
-                TextEntry::make('deleted_at')
-                    ->dateTime(),
-                TextEntry::make('created_at')
-                    ->dateTime(),
-                TextEntry::make('updated_at')
-                    ->dateTime(),
+
+                Section::make('Data Recrutment Mahasiswa')
+                    ->description('Mohon DI Cek Kembali Data Anda')
+                    ->icon(Heroicon::UserCircle)
+                    ->schema([
+                        TextEntry::make('nim')
+                            ->label('NIM')
+                            ->numeric(),
+                        TextEntry::make('name')
+                            ->label('Nama Lengkap'),
+                        TextEntry::make('semester')
+                            ->label('Semester'),
+                        TextEntry::make('email')
+                            ->label('Email'),
+                        TextEntry::make('instagram')
+                            ->label('Instagram')
+                            ->url(fn ($record) => $record->instagram, true)
+                            ->badge('info'),
+                        TextEntry::make('no_wa')
+                            ->label('No WhatsApp')
+                            ->url(fn ($record) => 'https://wa.me/' . ltrim($record->no_wa, '0'), true)
+                            ->badge('success'),
+                        ImageEntry::make('ektm')
+                            ->label('E-KTM')
+                            ->disk('public')
+                            ->columnSpanFull(),
+                        TextEntry::make('status.name')
+                            ->label('Status Recruitment'),
+                        TextEntry::make('cv')
+                            ->label('Curriculum Vitae / CV')
+                            ->url(fn ($record) => $record->cv, true)
+                            ->badge('info'),
+                        TextEntry::make('branch.name')
+                            ->label('Cabang / DPC'),
+                        ImageEntry::make('follow_dpc')
+                            ->label('Follow DPC')
+                            ->disk('public'),
+                    ])->columns(2)->columnSpanFull(),
+                
+                Section::make('Timestamps')
+                    ->icon(Heroicon::Clock)
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('created_at')
+                                    ->label('Created At')
+                                    ->dateTime('d/F/Y H:i'),
+                                TextEntry::make('updated_at')
+                                    ->label('Last Updated')
+                                    ->dateTime('d/F/Y H:i'),
+                            ]),
+                ])->columnSpanFull()->collapsible(),
             ]);
     }
 }
