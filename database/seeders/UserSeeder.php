a94fe571cca400c72ec9eec2dde396dc9c10750b
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-03 13:02:37 +0700

    fif spatie role seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -14,6 +16,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $superAdminRole = Role::firstOrCreate(['name' => 'super-admin']);
+
+        $policyFiles = File::files(app_path('Policies'));
+
+        foreach ($policyFiles as $file) {
+            $className = pathinfo($file, PATHINFO_FILENAME);
+            $resource = strtolower(str_replace('Policy', '', $className));
+
+            $actions = [
+                'view_any', 'view', 'create', 'update', 'delete', 
+                'delete_any', 'force_delete', 'force_delete_any', 
+                'restore', 'restore_any', 'replicate', 'reorder'
+            ];
+
+            foreach ($actions as $action) {
+                $permissionName = "{$action}_{$resource}";
+                $permission = Permission::firstOrCreate(['name' => $permissionName]);
+                $superAdminRole->givePermissionTo($permission);
+            }
+        }
+
         $user = User::create([
             "id" => 1,
             'name' => 'Admin',
@@ -22,7 +45,6 @@ class UserSeeder extends Seeder
             'password' => bcrypt('P@ssw0rdHimsi'),  
         ]);
 
-        $role = Role::firstOrCreate(['name' => 'super-admin']);
-        $user->assignRole($role->name); 
+        $user->assignRole($superAdminRole->name);
     }
 }
