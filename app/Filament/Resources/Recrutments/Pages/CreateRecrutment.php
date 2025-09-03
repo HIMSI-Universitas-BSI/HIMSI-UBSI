4a1809b711f01c348ec16a173f49b9d75cf4d380
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-03 21:29:06 +0700

    buat redirect ke wa pas create recrutment

--- app/Filament/Resources/Recrutments/Pages/CreateRecrutment.php
+++ app/Filament/Resources/Recrutments/Pages/CreateRecrutment.php
@@ -8,4 +8,14 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRecrutment extends CreateRecord
 {
     protected static string $resource = RecrutmentResource::class;
+
+    // method buat redirect ke grup wa saat create
+    protected function getRedirectUrl(): string
+    {
+        if ($this->record && $this->record->branch) {
+            return $this->record->branch->grup_wa;
+        }
+
+        return $this->getResource()::getUrl('index');
+    }
 }
