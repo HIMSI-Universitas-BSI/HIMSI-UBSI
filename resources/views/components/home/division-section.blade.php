96a04de1fe84a12ec38918e67fe33326fb583557
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-04 23:48:18 +0700

    ambil data divisi di section

--- resources/views/components/home/division-section.blade.php
+++ resources/views/components/home/division-section.blade.php
@@ -1,73 +1,35 @@
 <section class="wrapper !bg-[#ffffff] ">
-    <div class="container pt-20 xl:pt-28 lg:pt-28 md:pt-28 pb-16 xl:pb-20 lg:pb-20 md:pb-20 !text-center">
-    <div class="flex flex-wrap mx-[-15px]">
-        <div class="md:w-10/12 xl:w-8/12 lg:w-8/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!ml-[16.66666667%] lg:!ml-[16.66666667%] md:!ml-[8.33333333%]">
-        <h2 class="!text-[.75rem] uppercase !text-[#aab0bc] !mb-3 !tracking-[0.02rem] !leading-[1.35]">What We Do?</h2>
-        <h3 class="!text-[calc(1.305rem_+_0.66vw)] font-bold xl:!text-[1.8rem] !leading-[1.3] !mb-10 xl:!px-10">The service we offer is specifically designed to meet your needs.</h3>
+    <div class="container py-10 lg:py-15 !text-center">
+        <div class="flex flex-wrap mx-[-15px]">
+            <div class="md:w-10/12 xl:w-8/12 lg:w-8/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!ml-[16.66666667%] lg:!ml-[16.66666667%] md:!ml-[8.33333333%]">
+                <h2 class="!text-[.75rem] uppercase !text-[#aab0bc] !mb-3 !tracking-[0.02rem] !leading-[1.35]">What We Do?</h2>
+                <h3 class="!text-[calc(1.305rem_+_0.66vw)] font-bold xl:!text-[1.8rem] !leading-[1.3] !mb-10 xl:!px-10">The service we offer is specifically designed to meet your needs.</h3>
+            </div>
+            <!-- /column -->
         </div>
-        <!-- /column -->
-    </div>
-    <!-- /.row -->
-    <div class="!relative">
-        <div class="shape !rounded-[50%] !bg-[#edf2fc] rellax !w-[6rem] !h-[6rem] !absolute z-[1]" data-rellax-speed="1" style="bottom: -0.5rem; right: -2.2rem; z-index: 0;"></div>
-        <div class="shape bg-dot yellow rellax !w-[6rem] !h-[7rem] !absolute opacity-50" data-rellax-speed="1" style="top: -0.5rem; left: -2.5rem; z-index: 0;"></div>
-        <div class="flex flex-wrap mx-[-15px] xl:mx-[-12.5px] lg:mx-[-12.5px] md:mx-[-12.5px] !mt-[-25px] !text-center">
-        <div class="md:w-6/12 lg:w-6/12 xl:w-3/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[12.5px] lg:!px-[12.5px] md:!px-[12.5px] max-w-full !mt-[25px]">
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)]">
-            <div class="card-body flex-[1_1_auto] p-[40px]">
-                <img src="../../assets/img/icons/lineal/search-2.svg" class="svg-inject icon-svg icon-svg-md !w-[2.6rem] !h-[2.6rem] !text-[#fab758] text-yellow !mb-3 m-[0_auto]" alt="image">
-                <h4>SEO Services</h4>
-                <p class="!mb-2">Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus cras justo.</p>
-                <a href="#" class="more hover !text-[#fab758] focus:!text-[#fab758] hover:!text-[#fab758]">Learn More</a>
+        <!-- /.row -->
+        <div class="!relative">
+            <div class="flex flex-wrap mx-[-15px] xl:mx-[-12.5px] lg:mx-[-12.5px] md:mx-[-12.5px] !mt-[-25px] !text-center">
+                
+                @foreach ( $divisi as $d )
+                    <div class="md:w-6/12 lg:w-6/12 xl:w-3/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[12.5px] lg:!px-[12.5px] md:!px-[12.5px] max-w-full !mt-[25px] ">
+                        <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)]">
+                            <div class="card-body flex-[1_1_auto] p-[20px]">
+                                <img src="{{ asset('storage/' . $d->logo) }}" class="svg-inject icon-svg icon-svg-md !w-[4.6rem] !h-[4.6rem] !text-[#fab758] text-yellow !mb-3 m-[0_auto]" alt="{{ $d->name }}">
+                                <h4>{{ $d->name }}</h4>
+                                <p class="!mb-2">{{ \Illuminate\Support\Str::limit($d->description, 80) }}</p>
+                                <a href="#" class="more hover !text-[#fab758] focus:!text-[#fab758] hover:!text-[#fab758]">Selengkapnya</a>
+                            </div>
+                            <!--/.card-body -->
+                        </div>
+                        <!--/.card -->
+                    </div>    
+                @endforeach
+
             </div>
-            <!--/.card-body -->
-            </div>
-            <!--/.card -->
+                <!--/.row -->
         </div>
-        <!--/column -->
-        <div class="md:w-6/12 lg:w-6/12 xl:w-3/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[12.5px] lg:!px-[12.5px] md:!px-[12.5px] max-w-full !mt-[25px]">
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)]">
-            <div class="card-body flex-[1_1_auto] p-[40px]">
-                <img src="../../assets/img/icons/lineal/browser.svg" class="svg-inject icon-svg icon-svg-md !w-[2.6rem] !h-[2.6rem] !text-[#e2626b] text-red !mb-3 m-[0_auto]" alt="image">
-                <h4>Web Design</h4>
-                <p class="!mb-2">Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus cras justo.</p>
-                <a href="#" class="more hover !text-[#e2626b] focus:!text-[#e2626b] hover:!text-[#e2626b]">Learn More</a>
-            </div>
-            <!--/.card-body -->
-            </div>
-            <!--/.card -->
-        </div>
-        <!--/column -->
-        <div class="md:w-6/12 lg:w-6/12 xl:w-3/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[12.5px] lg:!px-[12.5px] md:!px-[12.5px] max-w-full !mt-[25px]">
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)]">
-            <div class="card-body flex-[1_1_auto] p-[40px]">
-                <img src="../../assets/img/icons/lineal/chat-2.svg" class="svg-inject icon-svg icon-svg-md !w-[2.6rem] !h-[2.6rem] !text-[#45c4a0] text-green !mb-3 m-[0_auto]" alt="image">
-                <h4>Social Engagement</h4>
-                <p class="!mb-2">Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus cras justo.</p>
-                <a href="#" class="more hover !text-[#45c4a0] focus:!text-[#45c4a0] hover:!text-[#45c4a0]">Learn More</a>
-            </div>
-            <!--/.card-body -->
-            </div>
-            <!--/.card -->
-        </div>
-        <!--/column -->
-        <div class="md:w-6/12 lg:w-6/12 xl:w-3/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[12.5px] lg:!px-[12.5px] md:!px-[12.5px] max-w-full !mt-[25px]">
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)]">
-            <div class="card-body flex-[1_1_auto] p-[40px]">
-                <img src="../../assets/img/icons/lineal/megaphone.svg" class="svg-inject icon-svg icon-svg-md !w-[2.6rem] !h-[2.6rem] !text-[#3f78e0] text-blue !mb-3  m-[0_auto]" alt="image">
-                <h4>Content Marketing</h4>
-                <p class="!mb-2">Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus cras justo.</p>
-                <a href="#" class="more hover !text-[#3f78e0]   hover:!text-[#3f78e0]">Learn More</a>
-            </div>
-            <!--/.card-body -->
-            </div>
-            <!--/.card -->
-        </div>
-        <!--/column -->
-        </div>
-        <!--/.row -->
-    </div>
-    <!-- /.relative -->
+        <!-- /.relative -->
     </div>
 </section>
 <!-- /section -->
