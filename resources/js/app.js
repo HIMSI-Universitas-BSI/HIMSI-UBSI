c21a837c7a6da1b25b40cc70699c484eeaee3cf3
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-04 23:05:18 +0700

    ubah divisi card di home

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import "../../public/assets/js/plugins";
+import "../../public/assets/js/theme";
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (typeof theme !== "undefined") {
+        theme.init();
+    }
+});
