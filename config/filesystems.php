a085ab2ebefe7f1a3e1fc576019c50f505207527
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-13 15:37:13 +0700

    tambah disk himsi

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,14 @@ return [
             'throw' => false,
         ],
 
+        'himsi' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/himsi'),
+            'url' => env('APP_URL', "http://localhost:8000") . '/storage/himsi',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
