d6b73d56473c18dbededfbc6fc2580d8db6a94cb
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-13 15:25:10 +0700

    buat fungsi convert di create blog

--- app/Filament/Resources/Blogs/Pages/CreateBlog.php
+++ app/Filament/Resources/Blogs/Pages/CreateBlog.php
@@ -2,10 +2,25 @@
 
 namespace App\Filament\Resources\Blogs\Pages;
 
-use App\Filament\Resources\Blogs\BlogResource;
+use App\Helpers\FileHelpers;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\Blogs\BlogResource;
 
 class CreateBlog extends CreateRecord
 {
     protected static string $resource = BlogResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (isset($data['banner'])) {
+            $data['banner'] = FileHelpers::convertWebp($data['banner'], "himsi");
+        }
+
+        if (isset($data['gambar']) && is_array($data['gambar'])) {
+            foreach ($data['gambar'] as $index => $gambar) {
+                $data['gambar'][$index] = FileHelpers::convertWebp($gambar, "himsi");
+            }
+        }
+        return $data;
+    }
 }
